b7a18f75f3c3131a1a51effd742ab70ccc74050b
El-hadi MEDEGHRI <doudido78@gmail.com>
2015-05-04 23:28:50 +0200

    modification du nom de la fonction bs et ajout d'une deuxime fonction pour le put

--- blackScholes.R
+++ blackScholes.R
@@ -1,7 +1,14 @@
-blackScholes<-function(S0,k,sigma,r,t){
+blackScholesCall<-function(S0,k,sigma,r,t){
 
 d1<-(log(S0/k)+(r+sigma^2/2)*t)/(sigma*sqrt(t))
 d2<-d1-sigma*sqrt(t)
 c0<-S0*pnorm(d1)-k*exp(-r*t)*pnorm(d2)
+
+}
+blackSholesPut<-function(S0,k,sigma,r,t){
+d1=(log(S0/k)+(r+sigma^2/2)*t)/(sigma*sqrt(t))
+d2=d1-sigma*sqrt(t)
+p0<--S0*pnorm(-d1)+k*exp(-r*t)*pnorm(-d2)
+
 
 }

ab7e3795253150e74b9d258e4ba181514f336aa4
El-hadi MEDEGHRI <doudido78@gmail.com>
2015-05-04 23:31:49 +0200

    ajout de commentaire et modification fonction bsput

--- blackScholes.R
+++ blackScholes.R
@@ -1,3 +1,4 @@
+#fonction de calcul de la valeur  du call selon la formule de black and scholes
 blackScholesCall<-function(S0,k,sigma,r,t){
 
 d1<-(log(S0/k)+(r+sigma^2/2)*t)/(sigma*sqrt(t))
@@ -5,9 +6,11 @@ d2<-d1-sigma*sqrt(t)
 c0<-S0*pnorm(d1)-k*exp(-r*t)*pnorm(d2)
 
 }
+
+#fonction de calcul de la valeur du  put selon la formule de black and scholes
 blackSholesPut<-function(S0,k,sigma,r,t){
-d1=(log(S0/k)+(r+sigma^2/2)*t)/(sigma*sqrt(t))
-d2=d1-sigma*sqrt(t)
+d1<-(log(S0/k)+(r+sigma^2/2)*t)/(sigma*sqrt(t))
+d2<-d1-sigma*sqrt(t)
 p0<--S0*pnorm(-d1)+k*exp(-r*t)*pnorm(-d2)
 
 
